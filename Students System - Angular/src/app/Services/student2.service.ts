80dc96d38b46720991551e140571bc916eff6f26
iAhmed-Ragab0 <iAhmedRagab0@gmail.com>
2023-07-05 00:05:06 +0300

    full demo Angular + Node.js API

---
+++ Students System - Angular/src/app/Services/student2.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Student2Model } from '../Models/student2-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Student2Service {
+
+  baseUrl="http://localhost:8080/students/" 
+  constructor(public http:HttpClient) { }
+
+  getAllStudents(){
+    return this.http.get<Student2Model[]>(this.baseUrl)
+  }
+
+  getById(id:number){
+    return this.http.get<Student2Model>(this.baseUrl+id)
+  }
+
+  addStudent(newStudent:Student2Model){
+    return this.http.post(this.baseUrl,newStudent)
+  }
+  deleteStudent(id:number){
+    return this.http.delete(this.baseUrl+id)
+  }
+
+  // props to bind on it in the edit component
+  public editedID1:any ;
+  public editedName:any ;
+  public editedImage:any ;
+
+  editStudent(id:number , editedStd : Student2Model){
+
+    console.log(editedStd)
+    return this.http.put(this.baseUrl +id ,editedStd)
+
+  }
+
+
+}
